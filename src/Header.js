c0e72590bb691f7406ccc2cf13fb712ff51abe81
ysajjan81 <ysajjan81@gmail.com>
2020-10-23 21:18:04 -0700

    added header

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import './App.css'
+import React, { Component } from 'react';
+class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {  }
+	}
+	render() { 
+		return ( 
+		<div  style = {{backgroundColor:'purple'}}>
+			{/* <img src = "https://image4.owler.com/logo/stackline_owler_20170129_010716_original.png" alt ="StackLine Logo" height="40" style ={{ margin:'20px'}}></img> */}
+			<img src = "stackline_logo.png" alt ="StackLine Logo" height="30" style ={{ margin:'20px'}}></img>
+		</div>
+		 );
+	}
+}
+ 
+export default Header;
