6bc8a02c98c038aa3cae83ca5b255e2c66628720
ysajjan81 <ysajjan81@gmail.com>
2020-10-26 01:53:17 -0700

    removed comments

--- src/reducers/ProductReducer.js
+++ src/reducers/ProductReducer.js
@@ -16,8 +16,6 @@ export default function productReducer(
 ) {
   switch (action.type) {
     case FETCH_PRODUCTS_BEGIN:
-      // Mark the state as "loading" so we can show a spinner or something
-      // Also, reset any errors. We're starting fresh.
       return {
         ...state,
         loading: true,
@@ -25,8 +23,6 @@ export default function productReducer(
       };
 
     case FETCH_PRODUCTS_SUCCESS:
-      // All done: set loading "false".
-      // Also, replace the items with the ones from the server
       return {
         ...state,
         loading: false,
@@ -34,9 +30,6 @@ export default function productReducer(
       };
 
     case FETCH_PRODUCTS_FAILURE:
-      // The request failed, but it did stop, so set loading to "false".
-      // Save the error, and we can display it somewhere
-      // Since it failed, we don't have items to display anymore, so set it empty.
       return {
         ...state,
         loading: true,
@@ -45,7 +38,6 @@ export default function productReducer(
       };
 
     default:
-      // ALWAYS have a default case in a reducer
       return state;
   }
 }
